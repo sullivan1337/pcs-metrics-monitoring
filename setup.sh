1c0a6c5a69db95a15fafc47fd50e59c356794a24
Kyle Butler <54778108+kyle9021@users.noreply.github.com>
2022-06-01 12:35:31 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,14 +16,16 @@ running() {
 
 
 # Docker Dependency Check
-if ! docker info > /dev/null 2>&1; then
-    echo "ERROR: docker is not available or not runnning."
-    echo "This script requires docker, please install and try again."
+if ! docker info > /dev/null 2>&1
+  then
+    printf '%s\n%s\n' "ERROR: docker is not available or not runnning." \
+                      "This script requires docker, please install and try again."
     exit 1
 fi
-if ! docker-compose version > /dev/null 2>&1; then
-    echo "ERROR: docker-compose is not available or not runnning."
-    echo "This script requires docker-compose, please install and try again."
+if ! docker-compose version > /dev/null 2>&1
+  then
+    printf '%s\n%s\n' "ERROR: docker-compose is not available or not runnning." \
+                      "This script requires docker-compose, please install and try again."
     exit 1
 fi
 
@@ -36,19 +38,19 @@ TL_CONSOLE_MATCH='^https\:\/\/(\w|\d|\.|\-|\_|\:|\/)+$'
 tl-var-check () {
 if [[ ! $TL_CONSOLE =~ $TL_CONSOLE_MATCH ]]
   then
-    echo "$TL_CONSOLE is not a valid value for TL_CONSOLE. Please recopy, verify, and run again"
+    printf '%s\n' "$TL_CONSOLE is not a valid value for TL_CONSOLE. Please recopy, verify, and run again"
     exit 1
 fi
 
 if [[ ! $TL_SECRETKEY =~ $TL_SECRETKEY_MATCH ]]
   then
-     echo "The Secret key is not valid. Please recopy, verify, and run again"
+     printf '%s\n' "The Secret key is not valid. Please recopy, verify, and run again"
      exit 1
 fi
 
 if [[ ! $TL_ACCESSKEY =~ $TL_ACCESSKEY_MATCH ]]
   then
-     echo "The Access Key is not valid. Please recopy, verify, and run again"
+     printf '%s\n' "The Access Key is not valid. Please recopy, verify, and run again"
      exit 1
 fi
 }
@@ -58,23 +60,43 @@ fi
 
 ENV_FILE_PATH="./collector/secrets.env"
 
-echo "enter your prisma access key id:"
-read -r -s  TL_ACCESSKEY
-echo "enter your prisma secret key id:"
+printf '%s\n' "enter your prisma access key id:"
+read -r  TL_ACCESSKEY
+printf '%s\n' "enter your prisma secret key id:"
 read -r -s  TL_SECRETKEY
-echo "enter your prisma cloud api url, like https://us-east1.cloud.twistlock.com/us-1-1111111"
+printf '%s\n' "enter your prisma cloud api url, like https://us-east1.cloud.twistlock.com/us-1-1111111"
 read -r TL_CONSOLE
 
 tl-var-check
 
-printf '%s\n' "#!/bin/sh" > $ENV_FILE_PATH
-printf '%s\n' "TL_CONSOLE=\"$TL_CONSOLE\"" >> $ENV_FILE_PATH
-printf '%s\n' "TL_ACCESSKEY=\"$TL_ACCESSKEY\"" >> $ENV_FILE_PATH
-printf '%s\n' "TL_SECRETKEY=\"$TL_SECRETKEY\"" >> $ENV_FILE_PATH
+
+TL_DOMAIN=$(printf '%s' "$TL_CONSOLE" | awk -F '\/' '{print $3}')
+TL_REGION=$(printf '%s' "$TL_CONSOLE" | awk -F '\/' '{print $4}')
+
+
+
+printf '%s\n' "Configuring prometheus.yml file"
+
+sed -i "s|prisma_cloud_compute_domain|$TL_DOMAIN|g" "./prometheus/prometheus.yml"
+sed -i "s|prisma_cloud_compute_region|$TL_REGION|g" "./prometheus/prometheus.yml"
+sed -i "s|PC_ACCESS_KEY|$TL_ACCESSKEY|g" "./prometheus/prometheus.yml"
+sed -i "s|PC_SECRET_KEY|$TL_SECRETKEY|g" "./prometheus/prometheus.yml"
+
+printf '%s\n' "-----------------------------------------"
+
+
+printf '%s\n' "Configuring env file"
+printf '%s\n%s\n%s\n%s\n' "#!/bin/sh" \
+       "TL_CONSOLE=\"$TL_CONSOLE\"" \
+       "TL_ACCESSKEY=\"$TL_ACCESSKEY\"" \
+       "TL_SECRETKEY=\"$TL_SECRETKEY\"" > $ENV_FILE_PATH
+
+printf '%s\n%s\n' "-----------------------------------------" \
+                  "Building collector container"
 
 docker build -t collector:dev ./collector/.
 
-
+printf '%s\n' "-----------------------------------------" 
 
 # Build Docker
 echo "Running Docker-Compose..."
